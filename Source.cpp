7a95fae1cfc6bf7152f1936d539ea36e7f451c83
jinhgkim <71912315+jinhgkim@users.noreply.github.com>
2021-06-10 06:54:02 -0700

    Add files via upload

---
+++ Source.cpp
@@ -0,0 +1,40 @@
+﻿#include<iostream>
+#include<windows.h> 
+using namespace std;
+
+int main()
+{
+	int hour, minute, second;
+
+	cout << "=================== Set timer ===================" << endl;
+	cout << endl;
+	cout << "HH: "; cin >> hour; cout << endl;
+	cout << "MM: "; cin >> minute; cout << endl;
+	cout << "SS: "; cin >> second; cout << endl;
+
+	while (true)
+	{
+		for (hour; hour >= 0; hour--)
+		{
+			for (minute; minute >= 0; minute--)
+			{
+				for (second; second >= 0; second--)
+				{
+					system("cls");
+					cout << "=================== Time left ===================" << endl;
+					cout << endl;
+					cout << hour << " : " << minute << " : " << second << endl;
+					Sleep(1000);
+				}
+				second = 59;
+			}
+			minute = 59;
+		}
+		cout << endl;
+		cout << "Time's up!!!" << endl;
+		break;
+	}
+	
+	system("Pause");
+	return 0;
+}
