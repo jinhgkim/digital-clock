673e9f04093d2aaa617e10ebc3b53f01b2f879df
jinhgkim <71912315+jinhgkim@users.noreply.github.com>
2021-06-10 07:00:10 -0700

    Add files via upload

---
+++ Source.cpp
@@ -0,0 +1,28 @@
+﻿#include<iostream>
+#include<windows.h> 
+using namespace std;
+
+int main()
+{
+	int hour = 0, minute = 0, second = 0;
+
+	while (true)
+	{
+		for (; ; hour++)
+		{
+			for (; minute < 60; minute++)
+			{
+				for (; second < 60; second++)
+				{
+					system("cls");
+					cout << "=================== Count-up ===================" << endl;
+					cout << endl;
+					cout << hour << " : " << minute << " : " << second << endl;
+					Sleep(1000);
+				}
+				second = 0;
+			}
+			minute = 0;
+		}
+	}
+}
